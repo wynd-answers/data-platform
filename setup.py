ca455c274d841ac85241ae63a14219a990efd856
mirk26 <morellato26@gmail.com>
2021-11-20 18:38:48 +0530

    setting cookiecutter and uploading notebooks of this week

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Realtime data apps for emmission data from ESA satellites',
+    author='mirk',
+    license='MIT',
+)
